c3f897a00157f631ee1d064ab176750eb14c600d
Novatrr <gamingpurposes010@gmail.com>
2026-02-12 19:56:24 +0700

    Add files via upload

---
+++ items.js
@@ -0,0 +1,17 @@
+const ITEMS = [
+  {
+    name: "MOAW/Waffle Launcher",
+    type: "Weapon",
+    rarity: 100,
+    power: 100,
+    image: "images/MOAW.jpg"
+  },
+  {
+    name: "Nuke Launcher",
+    type: "Weapon",
+    rarity: 60,
+    power: 75,
+    image: "images/nuke_launcher.png"
+  }
+
+];
